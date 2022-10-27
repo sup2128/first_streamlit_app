4915acaf317d1eee19b0aada98dab740f4ae21ca
sup2128 <37988032+sup2128@users.noreply.github.com>
2022-10-27 09:37:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 print(streamlit.title('My Parents New Healthy Dinner'))
+
+print(streamlit.title('Breakfast Menu'))
+print(streamlit.title('Aloo Porota'))
+print(streamlit.title('Samosa'))
+print(streamlit.title('Honey'))
