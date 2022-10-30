816d0d795474057447af8ec0bd2bd7dd903ee2e6
sup2128 <37988032+sup2128@users.noreply.github.com>
2022-10-29 21:32:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,12 @@ import snowflake.connector
 from urllib.error import URLError
 streamlit.title('My Parents New Healthy Dinner')
 
+def get_fruityvice_data(fruit_choice):
+  fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/"+fruit_choice)
+  streamlit.header("FruityVice Advice")
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -16,17 +22,21 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
+
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
-fruit_choice=streamlit.text_input('which fruit you want?','kiwi')
+try:
+  fruit_choice=streamlit.text_input('which fruit you want?')
+  if not fruit_choice:
+    streamlit.error('Please select fruit')
+  else:
+    fruityvice_normalized = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(fruityvice_normalized)
+    
 
-fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/"+fruit_choice)
-streamlit.header("FruityVice Advice")
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 fruit=streamlit.text_input('which fruit you want?','apple')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
